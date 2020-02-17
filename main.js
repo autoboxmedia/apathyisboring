d2fca925b9bdb0e8164a912b6b76ce2c328bdb47
Jedd <jedd@autoboxmedia.com>
2020-02-17 14:36:57 -0800

    local devmode

--- main.js
+++ main.js
@@ -27,4 +27,41 @@ $(document).ready(function() {
 		hideClass: 'fx-fadeOut',
     videoAutoPlay: 'true'
 	});
+	
+	aib.init();
+	
+	
 });
+
+
+aib = {
+		init: function() {
+			this.devMode();
+		},
+		
+		devMode: function(){
+			var localDev = false;
+			var localUrl = 'https://aib:8890',
+				localCss = '/theme.css',
+				remoteCss = 'theme.scss';
+			
+			$.ajax(localUrl + localCss, {
+				statusCode: {
+					200: function(r) {
+						localDev = true;
+					}
+				},
+				complete: function(r, status) {
+					if (status != 'success') return;
+					$('head link[href*="'+ remoteCss +'"]').remove();
+					$('head').append('<link rel="stylesheet"  href="'+ localUrl + localCss +'" type="text/css" media="all">')
+					//.append("<script src=\"http://localhost:35729/livereload.js?snipver=1\"></script>");
+					console.log('AiB: local css swapped in');
+				}
+			});
+		}
+	}
+
+
+
+	
